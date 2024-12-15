57aadea1c8789ebe1f2a36448b98295a19487fc8
tandouridev <simotandori@icloud.com>
2024-12-15 14:51:15 +0100

    simo

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'export', images: {
+        loader: 'custom',
+        loaderFile: './loader.js',
+    },
+};
 
 export default nextConfig;
